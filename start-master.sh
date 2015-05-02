14922d6ce7a60e604e07dbd0c39ec259371e54e3
Toni Bagur <toni.bagur@gmail.com>
2015-05-02 17:21:56 +0200

    .

--- start-master.sh
+++ start-master.sh
@@ -1,2 +1,2 @@
 CURRENT_PATH=$(pwd)
-docker run -d -p 5432:5432 -v $CURRENT_PATH:/tmp/postgresql -v $CURRENT_PATH/init-master.sh:/docker-entrypoint-initdb.d/init.sh --name postgres postgres 
+docker run -d -p 5432:5432 -v $CURRENT_PATH:/tmp/postgresql -v $CURRENT_PATH/init-master.sh:/docker-entrypoint-initdb.d/init.sh -v /home/coneptum/201502181900.dump:/restore.dump --name postgres postgres 
